1041a826b9b120e8ae4777d41d0f6febc60c9bfc
AbdouFl <abderrahimfilaliofficial@gmail.com>
2023-11-16 02:13:27 +0100

    main function

--- main.c
+++ main.c
@@ -15,9 +15,9 @@ void open_error(const char *program_name, const char *file_name)
 {
 	/* Print program name, error message, file name, and exit with status 2 */
 	_eputs(program_name);
-	_eputs(": 0: cannot open ");
+	_eputs(": 0: Can't open ");
 	_eputs(file_name);
-	_eputs(": No such file\n");
+	_eputs("\n");
 	_eputchar(BUFFER_FLUSH);
 	exit(2);
 }
